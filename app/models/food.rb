5a5c76e53704c638f72b88e876d842db55d499d2
Habtamu210 <beleyaha@gmail.com>
2023-06-08 01:03:56 -0700

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,7 +6,6 @@ class Food < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :expiration_date, presence: true
 
-
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :recipe_foods, foreign_key: 'foods_id', dependent: :destroy
 end
